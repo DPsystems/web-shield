80f35dbc47488421ff25d0a891bbe9d2e04ecd44
dpsystems <dolson803@gmail.com>
2022-08-22 10:28:48 -0500

    Updates

---
+++ RESTART.web-shield.sh
@@ -0,0 +1,11 @@
+# This script is used for when you have web-shield already running and have done a
+# git pull and retrived an updated list
+# this will flush the existing config and rebuild the system
+
+# remove iptables reference
+./webshield-set-iptables.sh del
+# clear existing list
+iptables flush web-shield
+# reload the whole system (NOTE: replace this with whatever startup command you're using if different)
+./START.web-shield.sh
+
